d827618358b19ad8a001d8e0caba8bbd6d366e13
Tisitha <tisitha007@gmail.com>
2025-04-11 20:44:17 +0530

    fix eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,15 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals", "next/typescript",
+    {
+      extends: ['next'],
+      rules: {
+        'react/no-unescaped-entities': 'off',
+        '@next/next/no-page-custom-font': 'off',
+      },
+    }
+  )
 ];
 
 export default eslintConfig;
